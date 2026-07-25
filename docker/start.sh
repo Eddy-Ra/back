41b2221d5ce30869bb7b007fad8bd75ddece186e
Eddy Randriamanantany <eddyrandriamanantany@gmail.com>
2026-07-25 18:16:40 +0300

    25-07-2026

--- docker/start.sh
+++ docker/start.sh
@@ -3,6 +3,9 @@ set -e
 
 PORT="${PORT:-10000}"
 
+# Remplace le placeholder de port par le port réel fourni par Render
+sed -i "s/__PORT__/$PORT/g" /etc/nginx/sites-available/default
+
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
